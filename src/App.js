a051bad8e2239763f1c89d61deb2a3b70c9d1d49
bhavyameghnani <meghnani.bhavya@gmail.com>
2022-08-17 12:53:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { recognition } from "./API/voicerecognition";
+import Welcome from "./components/Home";
+import "./App.css";
+import Contact from "./components/Navbar";
 
-function App() {
+const App = () => {
+  const history = useNavigate();
+  const [stopReco, setStopReco] = useState(false);
+
+  recognition.onresult = (event) => {
+    const command = event.results[0][0].transcript;
+
+    if (command.includes("go to") || command.includes("navigate to")) {
+      if (command.includes("home") || command.includes("index")) {
+        history("/");
+      } else if (
+        command.includes("contact") ||
+        command.includes("contact us")
+      ) {
+        history("/contact");
+      } else if (
+        command.includes("tutorials") ||
+        command.includes("tutorial")
+      ) {
+        history("/tutorials");
+      } else if (command.includes("about") || command.includes("about us")) {
+        history("/about");
+      }
+    } else if (
+      command.includes("stop listening") ||
+      command.includes("stop recognition") ||
+      command.includes("stop recognizing") ||
+      command.includes("stop voice controlling") ||
+      command.includes("stop voice control")
+    ) {
+      recognition.stop();
+      setStopReco(true);
+    }
+  };
+
+  recognition.onend = () => {
+    if (!stopReco) {
+      recognition.start();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header" >
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
